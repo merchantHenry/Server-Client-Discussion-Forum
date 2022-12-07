5a4a4d6a236b7cc163cf6edd96d67605c6f718bb
merchantHenry <103093287+merchantHenry@users.noreply.github.com>
2022-12-06 21:53:21 -0500

    Create RevisedTeacherAccount.java

---
+++ RevisedTeacherAccount.java
@@ -0,0 +1,28 @@
+/**
+ * RevisedTeacherAccount.java
+ *
+ * Organizes information about teacher objects based on user input and server output, and allows
+ * teacher-oriented actions to be performed by the teacher user.
+ *
+ * @version April 22, 2022
+ */
+
+public class RevisedTeacherAccount extends RevisedAccount {
+    private String exists;
+
+    //for reading from file
+    RevisedTeacherAccount(String username, String password, String exists) {
+        super(username, password, exists);
+    }
+
+    //for when user creates new teacher account
+    RevisedTeacherAccount(String username, String password) {
+        super(username, password);
+        this.exists = "true";
+    }
+
+    public String toString() {
+        return super.toString() + " [account_type]:teacher";
+    }
+
+}
